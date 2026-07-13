6ffd7d13c2b8832726c567354064554da0dfb4ed
ManjotKaur14 <m04321665@gmail.com>
2026-07-13 14:38:06 +0530

    Time: 2 ms (99.3%), Space: 47.4 MB (10.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,9 +4,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int diff=target-nums[i];
             if(map.containsKey(diff)){
-                
                 return new int[]{map.get(diff),i};
-            }
+            } 
             map.put(nums[i],i);
         }
         return new int[]{-1,-1};

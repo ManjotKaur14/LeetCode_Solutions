aa35a3c561bc8750b7bb4723505bad862601a562
ManjotKaur14 <m04321665@gmail.com>
2026-07-21 22:46:29 +0530

    Time: 3 ms (86.86%), Space: 43.6 MB (15.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+                for(char ch:s.toCharArray()){
+                    if(ch=='[' || ch=='(' || ch=='{'){
+                    
+                    st.push(ch);
+                    }
+                else{
+                    if(st.isEmpty()){
+                        return false;
+                    }
+                    char c= st.pop();
+                    if(c=='(' && ch !=')' || c=='[' && ch !=']' ||c=='{' && ch !='}'){
+                        return false;
+                    }
+                }
+
+    }
+                return st.isEmpty();
+
+            }
+}
